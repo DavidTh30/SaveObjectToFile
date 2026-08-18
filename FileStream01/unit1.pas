4ba809f7644ad2117dbfe96e2d9391249defe2d0
Neramit <DavidTh30@gmail.com>
2026-08-19 03:30:23 +0700

    Add files via upload

--- FileStream01/unit1.pas
+++ FileStream01/unit1.pas
@@ -8,6 +8,9 @@ uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, streamex;
 
 type
+  A_Byte = array of Byte;
+
+type
 
   { TForm1 }
 
@@ -26,7 +29,6 @@ type
     Button9: TButton;
     Label1: TLabel;
     Label2: TLabel;
-    Label3: TLabel;
     procedure Button10Click(Sender: TObject);
     procedure Button11Click(Sender: TObject);
     procedure Button12Click(Sender: TObject);
@@ -54,6 +56,20 @@ implementation
 {$R *.lfm}
 
 { TForm1 }
+procedure BufferToFile(Buffer_: A_Byte; File_ : string);
+var
+  FileStream: TFileStream;
+begin
+  FileStream := TFileStream.Create(File_, fmCreate);
+  try
+    FileStream.WriteBuffer(Buffer_[0], length(Buffer_)); //Save both stucture and value
+    //showmessage(length(Buffer_).Tostring);
+  finally
+    FileStream.Free;
+  end;
+
+end;
+
 procedure SaveTextAndIntegerToStream(const AFileName: string; const MyValue: Integer);
 var
   FS: TFileStream;
@@ -87,7 +103,7 @@ begin
   try
     if FileStream.Size > 0 then
     begin
-      // Allocate the exact size of the file to the buffer
+      Buffer := Default(A_Byte);
       SetLength(Buffer, FileStream.Size);
 
       // Read everything directly into the buffer memory reference
@@ -112,7 +128,6 @@ begin
 
   FileStream := TFileStream.Create(FileName, fmOpenRead);
   try
-    // Keep reading until the end of the file stream is reached
     while FileStream.Position < FileStream.Size do
     begin
       BytesRead := FileStream.Read(Buffer, BufferSize);
@@ -145,10 +160,24 @@ var
   FileStream: TFileStream;
   Buffer1: array of Byte; // Dynamic array
   Buffer2: array of Byte; // Dynamic array
-  i:integer;
   Txt_:string;
   MyValue:integer;
 begin
+  if Label1 = nil then
+  begin
+    Label1 := Tlabel.Create(self);
+    Label1.Left:=336;
+    Label1.Top:=232;
+    Label1.Parent := Form1;
+  end;
+  if Label2 = nil then
+  begin
+    Label2 := Tlabel.Create(self);
+    Label2.Left:=336;
+    Label2.Top:=248;
+    Label2.Parent := Form1;
+  end;
+
   if Label1 = nil then exit;
   if Label2 = nil then exit;
 
@@ -176,6 +205,7 @@ begin
   try
     if FileStream.Size > 0 then
     begin
+      Buffer1 := Default(A_Byte);
       SetLength(Buffer1, FileStream.Size);
       FileStream.ReadBuffer(Buffer1[0], FileStream.Size);
     end;
@@ -187,6 +217,7 @@ begin
   try
     if FileStream.Size > 0 then
     begin
+      Buffer2 := Default(A_Byte);
       SetLength(Buffer2, FileStream.Size);
       FileStream.ReadBuffer(Buffer2[0], FileStream.Size);
     end;
@@ -197,14 +228,15 @@ begin
   FileStream := TFileStream.Create('test.bin', fmCreate);
   try
     FileStream.Seek(10, soCurrent);
-    Txt_:='[Section1]';
+    Txt_:='[Object1]';
     MyValue:=length(Buffer1);
     FileStream.WriteBuffer(Pointer(Txt_)^, length(Txt_)); //Save only string no stucture
     FileStream.WriteBuffer(MyValue, SizeOf(MyValue)); //Save both stucture and value
     FileStream.WriteBuffer(Buffer1[0], length(Buffer1)); //Save both stucture and value
+    //showmessage(length(Buffer1).ToString);
 
     FileStream.Seek(10, soCurrent);
-    Txt_:='[Section2]';
+    Txt_:='[Object2]';
     MyValue:=length(Buffer2);
     FileStream.WriteBuffer(Pointer(Txt_)^, length(Txt_)); //Save only string no stucture
     FileStream.WriteBuffer(MyValue, SizeOf(MyValue)); //Save both stucture and value
@@ -221,37 +253,67 @@ procedure TForm1.Button12Click(Sender: TObject);
 var
   FileStream: TFileStream;
   Buffer: array of Byte; // Dynamic array
+  Buffer_: A_Byte; // Dynamic array
   Arr1: array of Byte; //Arr1: array[1..ArraySize] of Byte = (1, 2, 3, 4, 5);
   s2:string;
   i:integer;
   ObjectSize:integer;
+  continue_:integer;
 begin
+  if Label1 <> nil then FreeAndNil(Label1);
+  if Label2 <> nil then FreeAndNil(Label2);
 
   if not FileExists('test.bin') then
   begin
-    showmessage('File not exists');
+    showmessage({$INCLUDE %LINE%}+': File not exists');
     Exit;
   end;
 
-  Arr1:=BytesOf('[Section1]');
+  ObjectSize := Default(Integer);
+  Arr1:=BytesOf('[Object1]');
   //SetString(s, PAnsiChar(@Arr1[0]), Length(Arr1)); //Array of byte to string
 
   FileStream := TFileStream.Create('test.bin', fmOpenRead);
   try
     if FileStream.Size > 0 then
     begin
+
+      Buffer := Default(A_Byte);
       SetLength(Buffer, FileStream.Size);
       FileStream.ReadBuffer(Buffer[0], FileStream.Size);
 
+      continue_:=0;
       for i := 0 to length(Buffer)-1 do
       begin
         Move(Buffer[i], Arr1[0], Length(Arr1)); //Transfer array of byte to array of byte
         SetString(s2, PAnsiChar(@Arr1[0]), Length(Arr1)); //Array of byte to string
-        if '[Section1]'=s2 then
+        if '[Object1]'=s2 then
         begin
-          Move(Buffer[i+11+1], ObjectSize, SizeOf(ObjectSize)); //Array of byte to integer
-          //Label1.Caption:=ObjectSize.ToString;
-          //Save Array of byte to Label.obj
+          Move(Buffer[i+Length(Arr1)], ObjectSize, SizeOf(ObjectSize)); //Array of byte to integer
+          //showmessage(ObjectSize.ToString);
+          Buffer_ := Default(A_Byte);
+          SetLength(Buffer_, ObjectSize);
+          Move(Buffer[i+Length(Arr1)+4], Buffer_[0], ObjectSize);
+          BufferToFile(Buffer_, 'Label.obj');
+          continue_:=i+Length(Arr1)+4;
+          break;
+        end;
+      end;
+
+      if continue_ > 0 then;
+      for i := continue_ to length(Buffer)-1 do
+      begin
+        Move(Buffer[i], Arr1[0], Length(Arr1)); //Transfer array of byte to array of byte
+        SetString(s2, PAnsiChar(@Arr1[0]), Length(Arr1)); //Array of byte to string
+        if '[Object2]'=s2 then
+        begin
+          Move(Buffer[i+Length(Arr1)], ObjectSize, SizeOf(ObjectSize)); //Array of byte to integer
+          //showmessage(ObjectSize.ToString);
+          Buffer_ := Default(A_Byte);
+          SetLength(Buffer_, ObjectSize);
+          Move(Buffer[i+Length(Arr1)+4], Buffer_[0], ObjectSize);
+          BufferToFile(Buffer_, 'Labe2.obj');
+          continue_:=i+Length(Arr1)+4;
           break;
         end;
       end;
@@ -261,6 +323,23 @@ begin
   finally
     FileStream.Free;
   end;
+
+  if (not FileExists('Label.obj')) or (not FileExists('Labe2.obj')) then
+  begin
+    showmessage({$INCLUDE %LINE%}+': File not exists');
+    exit;
+  end;
+
+  if Label1 <> nil then FreeAndNil(Label1);
+  Label1:= ReadComponentResFile('Label.obj',nil) as Tlabel;
+  Label1.Parent := Form1;
+
+  if Label2 <> nil then FreeAndNil(Label2);
+  Label2:= ReadComponentResFile('Labe2.obj',nil) as Tlabel;
+  Label2.Parent := Form1;
+
+  DeleteFile('Label.obj');
+  DeleteFile('Labe2.obj');
 end;
 
 procedure TForm1.Button2Click(Sender: TObject);
@@ -268,11 +347,27 @@ var
   FS: TFileStream;
   MyInteger: Integer;
 begin
+
+  if Label1 = nil then
+  begin
+    Label1 := Tlabel.Create(self);
+    Label1.Left:=336;
+    Label1.Top:=232;
+    Label1.Parent := Form1;
+  end;
+  if Label2 = nil then
+  begin
+    Label2 := Tlabel.Create(self);
+    Label2.Left:=336;
+    Label2.Top:=248;
+    Label2.Parent := Form1;
+  end;
+
   if Label1 = nil then exit;
   if Label2 = nil then exit;
 
   Label1.Caption:='Not found';
-  Label1.Caption:='Not found';
+  Label2.Caption:='Not found';
 
   if not FileExists('test.bin') then
   begin
@@ -300,11 +395,26 @@ var
   List: TStringList;
   FoundIndex: Integer;
 begin
+  if Label1 = nil then
+  begin
+    Label1 := Tlabel.Create(self);
+    Label1.Left:=336;
+    Label1.Top:=232;
+    Label1.Parent := Form1;
+  end;
+  if Label2 = nil then
+  begin
+    Label2 := Tlabel.Create(self);
+    Label2.Left:=336;
+    Label2.Top:=248;
+    Label2.Parent := Form1;
+  end;
+
   if Label1 = nil then exit;
   if Label2 = nil then exit;
 
   Label1.Caption:='Not found';
-  Label1.Caption:='Not found';
+  Label2.Caption:='Not found';
 
   if not FileExists('test.bin') then
   begin
@@ -333,13 +443,28 @@ var
   Stream: TFileStream;
   Reader: TStreamReader;
   Line: String;
-  Found: Boolean;
+  //Found: Boolean;
 begin
+  if Label1 = nil then
+  begin
+    Label1 := Tlabel.Create(self);
+    Label1.Left:=336;
+    Label1.Top:=232;
+    Label1.Parent := Form1;
+  end;
+  if Label2 = nil then
+  begin
+    Label2 := Tlabel.Create(self);
+    Label2.Left:=336;
+    Label2.Top:=248;
+    Label2.Parent := Form1;
+  end;
+
   if Label1 = nil then exit;
   if Label2 = nil then exit;
 
   Label1.Caption:='Not found';
-  Label1.Caption:='Not found';
+  Label2.Caption:='Not found';
 
   if not FileExists('test.bin') then
   begin
@@ -350,7 +475,7 @@ begin
   Stream := TFileStream.Create('test.bin', fmOpenRead or fmShareDenyWrite);
   Reader := TStreamReader.Create(Stream);
   try
-    Found := False;
+    //Found := False;
     while not Reader.Eof do
     begin
       Line := Reader.ReadLine;
@@ -377,17 +502,34 @@ var
   TargetValue, CurrentValue: Integer;
   Found: Boolean;
 begin
+  if Label1 = nil then
+  begin
+    Label1 := Tlabel.Create(self);
+    Label1.Left:=336;
+    Label1.Top:=232;
+    Label1.Parent := Form1;
+  end;
+  if Label2 = nil then
+  begin
+    Label2 := Tlabel.Create(self);
+    Label2.Left:=336;
+    Label2.Top:=248;
+    Label2.Parent := Form1;
+  end;
+
   if Label1 = nil then exit;
   if Label2 = nil then exit;
 
   Label1.Caption:='Not found';
-  Label1.Caption:='Not found';
+  Label2.Caption:='Not found';
 
   if not FileExists('test.bin') then
   begin
     showmessage('File not exists');
     Exit;
   end;
+
+  CurrentValue := Default(Integer);
 
   TargetValue := 1633973089; //'asdasd'
   Found := False;
@@ -411,8 +553,8 @@ begin
 end;
 
 procedure TForm1.Button6Click(Sender: TObject);
-const
-  ArraySize = 5;
+//const
+//  ArraySize = 5;
 var
   FileStream: TFileStream;
   Buffer: array of Byte; // Dynamic array
@@ -422,9 +564,26 @@ var
   s2:string;
   i2:integer;
 begin
+  if Label1 = nil then
+  begin
+    Label1 := Tlabel.Create(self);
+    Label1.Left:=336;
+    Label1.Top:=232;
+    Label1.Parent := Form1;
+  end;
+  if Label2 = nil then
+  begin
+    Label2 := Tlabel.Create(self);
+    Label2.Left:=336;
+    Label2.Top:=248;
+    Label2.Parent := Form1;
+  end;
+
+  if Label1 = nil then exit;
+  if Label2 = nil then exit;
 
   Label1.Caption:='Not found';
-  Label1.Caption:='Not found';
+  Label2.Caption:='Not found';
 
   if not FileExists('test.bin') then
   begin
@@ -432,6 +591,7 @@ begin
     Exit;
   end;
 
+  i2 := Default(Integer);
   Arr1:=BytesOf('[Section1]');
   SetString(s, PAnsiChar(@Arr1[0]), Length(Arr1)); //Array of byte to string
 
@@ -445,6 +605,7 @@ begin
   try
     if FileStream.Size > 0 then
     begin
+      Buffer := Default(A_Byte);
       SetLength(Buffer, FileStream.Size);
       FileStream.ReadBuffer(Buffer[0], FileStream.Size);
 
@@ -492,11 +653,26 @@ var
   Buffer: array of Byte; // Dynamic array
   i:integer;
 begin
+  if Label1 = nil then
+  begin
+    Label1 := Tlabel.Create(self);
+    Label1.Left:=336;
+    Label1.Top:=232;
+    Label1.Parent := Form1;
+  end;
+  if Label2 = nil then
+  begin
+    Label2 := Tlabel.Create(self);
+    Label2.Left:=336;
+    Label2.Top:=248;
+    Label2.Parent := Form1;
+  end;
+
   if Label1 = nil then exit;
   if Label2 = nil then exit;
 
   Label1.Caption:='Not found';
-  Label1.Caption:='Not found';
+  Label2.Caption:='Not found';
 
   if not FileExists('test.bin') then
   begin
@@ -504,10 +680,12 @@ begin
     Exit;
   end;
 
+  i := Default(Integer);
   FileStream := TFileStream.Create('test.bin', fmOpenRead);
   try
     if FileStream.Size > 0 then
     begin
+      Buffer := Default(A_Byte);
       SetLength(Buffer, FileStream.Size);
       FileStream.ReadBuffer(Buffer[0], FileStream.Size);
 
@@ -516,8 +694,7 @@ begin
 
       //0..3 = Stucture of integer
       //+10 = Seek 10 byte
-      //+1 = start next Stucture of integer
-      Move(Buffer[3+10+1], i, SizeOf(i));
+      Move(Buffer[4+10], i, SizeOf(i));
       Label2.Caption:=i.ToString;
 
     end;
@@ -555,7 +732,7 @@ begin
   if Label2 <> nil then FreeAndNil(Label2);
 end;
 
-end.
-
 initialization
-  RegisterClasses([TForm1,TLabel,TCustomLabel,TImage]);
+  RegisterClasses([TForm1,TLabel,TCustomLabel]);
+
+end.
